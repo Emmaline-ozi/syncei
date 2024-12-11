6a8d94743ede7210941060fb67bf27edfa4be6a6
oziGozi <ngoziogoteni@gmail>
2024-12-11 09:01:58 +0100

    convert to php-tst1

---
+++ index.php
@@ -0,0 +1,380 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Quattrocento+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="shared/reset.css">
+        <link rel="stylesheet" href="shared/nav.css">
+        <link rel="stylesheet" href="styles/index.css">
+        <link rel="stylesheet" href="shared/footer.css">
+        <!-- Favicon for browser tabs (standard sizes) -->
+         <link rel="icon" href="imgs/icon/favicon-16.png" sizes="16x16" type="image/png">
+        <link rel="icon" href="imgs/icon/favicon-32.png" sizes="32x32" type="image/png">
+        
+        <!-- Larger icons for high-resolution displays and mobile devices -->
+        <link rel="icon" href="imgs/icon/favicon-48.png" sizes="48x48" type="image/png">
+        <link rel="icon" href="imgs/icon/favicon-96.png" sizes="96x96" type="image/png">
+        <link rel="icon" href="imgs/icon/favicon-180.png" sizes="180x180" type="image/png">
+    
+        <!-- Apple Touch Icon (specifically for iOS home screen shortcuts) -->
+        <link rel="apple-touch-icon" href="imgs/icon/favicon-180.png" sizes="180x180">
+    
+        <!-- Android Chrome icons (especially for shortcuts or PWA homescreen icons) -->
+        <link rel="icon" href="imgs/icon/favicon-512.png" sizes="512x512" type="image/png">
+
+    <title>Syncei || Homepage </title>
+</head>
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/shared/nav.html'; ?>
+<div id="page">
+     <section class="hero">
+        <div class="vid">
+            <div class="holder">
+                <video autoplay muted loop playinline poster="/imgs/icon/favicon-512.png">
+                    <source src="videos/m5.mp4" type="video/mp4">
+                </video>
+                <div class="wrapper-vid">
+                    <div class="copy slide-right">
+                        <p>Enter — <span>into</span> — terminal</p>
+                        <div class="mix">
+                            <img src="imgs/svg/pointer.svg" alt="logo">
+                        </div>
+                    </div>
+                </div>
+                <div class="blocks">
+                    <div class="block block-left"></div>
+                    <div class="block block-right"></div>
+                </div>                  
+                <div class="letters">
+                    <div class="row top">
+                        <div class="letter top-left">
+                            <img src="imgs/svg/left-side.svg" alt="logo">
+                        </div>
+                    </div>
+                    <div class="row bottom">
+                        <div class="letter go-right">
+                            <img src="imgs/svg/top_half.svg" alt="logo">
+                            <div class="bottom-right">
+                                <img src="imgs/svg/bottom_half.svg" alt="logo">   
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>        
+        </div>  
+    </section>
+    <!-- infinite loop -->
+    <div class="logos">
+        <div class="logos-slide">
+            <span>3D Render Constructions</span>
+            <span>Digital Product Design</span>
+            <span>Branding & Identity</span>
+            <span>Motion & Illustration</span>
+            <span>Website & Mobile App Design</span>
+            <span>MVp Building</span>
+            <span>SEO & Backend management</span>
+            <span>Website Development</span>
+            <span>Product & Concept Discovery</span>
+            <span>Artificial Intelligence Employment</span>
+        </div>
+  
+        <div class="logos-slide">
+            <span>3D Render Constructions</span>
+            <span>Digital Product Design</span>
+            <span>Branding & Identity</span>
+            <span>Motion & Illustration</span>
+            <span>Website & Mobile App Design</span>
+            <span>MVp Building</span>
+            <span>SEO & Backend management</span>
+            <span>Website Development</span>
+            <span>Product & Concept Discovery</span>
+            <span>Artificial Intelligence Employment</span>
+        </div>
+      </div>
+    <!-- interact | us -->
+    <section class="inter">
+        <div class="graphic">
+            <div>
+                <dotlottie-player
+                src="https://lottie.host/8ffb8a34-6548-4d89-bd8e-a8e401703b3e/GsMpvkpQww.lottie"
+                background="transparent"
+                speed="0.5"
+                style="width: 300px; height: 300px"
+                loop
+                autoplay
+                ></dotlottie-player>
+            </div>
+        </div>
+        <article class="dsp">
+            <h1>design, develop & solve</h1>
+            <h3>
+                Incorporate designs, development & solutions that will give your brand a twist.
+            </h3>
+            <p>
+                We are a team of designers, developers, and innovators who are passionate about creating unique and effective. Our services are designed to help you achieve your goals and take your business to the next level. We offer a wide range of services, including design, development, and solutions that will give your brand a twist. Our team is dedicated to delivering high-quality work that meets your needs and exceeds your expectations. We are committed to providing exceptional customer service and building long-term relationships with our clients. We are passionate about what we do and are dedicated to helping you achieve your goals.  
+            </p>
+            <div class="btm">
+                <a class="offer" href="#">
+                    What we offer
+                </a>
+
+                <a class="learn-more" href="#">
+                <span class="circle" aria-hidden="true">
+                  <span class="icon arrow"></span>
+                </span>
+                <span class="button-text">Who we are</span>
+                </a>     
+            </div>
+            
+        </article>
+    </section>  
+    <!-- projects -->
+     <section class="projects">
+        <div class="divider">
+            <svg width="135" height="134" viewBox="0 0 135 134" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M34.2326 100.248H0.965332V98.1683H33.1931V1.03963H35.2723V99.208C35.2723 99.7278 34.8267 100.248 34.2326 100.248Z" fill="white"/>
+                <path d="M1.03958 133.441C0.891062 133.441 0.742547 133.441 0.66829 133.366C0.297003 133.218 0 132.847 0 132.401V1.0396C0 0.44554 0.445517 0 1.03958 0H133.96C134.406 0 134.777 0.222768 134.926 0.668312C135.074 1.0396 135 1.48514 134.703 1.78217L1.78215 133.218C1.55938 133.366 1.26235 133.441 1.03958 133.441ZM2.0792 2.0792V129.95L131.436 2.0792H2.0792Z" fill="#dddbff"/>
+                <path d="M33.5755 0.269618L0.34436 32.9878L1.80313 34.4695L35.0343 1.75126L33.5755 0.269618Z" fill="white"/>
+                <path d="M66.7504 0.295983L0.288086 65.7323L1.74685 67.214L68.2091 1.77762L66.7504 0.295983Z" fill="white"/>
+                <path d="M100.767 32.6733H1.03955V34.7525H100.767V32.6733Z" fill="white"/>
+                <path d="M67.4999 67.5H1.03955V65.4208H66.4604V1.03963H68.5396V66.4604C68.5396 67.0545 68.094 67.5 67.4999 67.5Z" fill="white"/>
+                <path d="M101.807 1.03963H99.7277V33.7872H101.807V1.03963Z" fill="white"/>
+                <path d="M100.018 0.248482L0.3125 98.916L1.77501 100.394L101.481 1.72638L100.018 0.248482Z" fill="#d890f9"/>
+                </svg>
+         </div>
+        <div class="header-proj">
+            <h3>
+                Syncei Projects <span>||</span>  our creative minds have no boundaries
+            </h3>
+            <a href="#">Take a look</a>              
+                                           
+            
+        </div>
+        <div class="grouped-grid">
+            <div class="sm-wd">
+                <div class="subprojects">
+                    <img src="imgs/resized-p/product-1.jpg" alt="" width="400" height="500">
+                    <span>Product Designs</span>
+                    <div class="curve">
+                    <a href="#">
+                      <button class="curve-box">                       
+                          <img src="imgs/svg/diagonal_arrow.svg" alt="">                       
+                      </button>
+                    </a> 
+                      
+                    </div>
+                </div>
+                <div class="subprojects">
+                    <img src="imgs/resized-p/interior-1.jpg" alt="" width="853.33" height="539.33">
+                    <span>3D Renders</span>
+                    <div class="curve">
+                      <a href="#">
+                        <button class="curve-box">                       
+                            <img src="imgs/svg/diagonal_arrow.svg" alt="">                       
+                        </button>
+                      </a> 
+                    </div>
+                  </div>              
+            </div>
+            <div class="wd-sm">
+                <div class="subprojects">
+                    <img src="imgs/resized-p/branding-1.jpg" alt="" width="300" height="300">
+                    <span>Branding</span>
+                    <div class="curve">
+                      <a href="#">
+                        <button class="curve-box">                       
+                            <img src="imgs/svg/diagonal_arrow.svg" alt="">                       
+                        </button>
+                      </a> 
+                    </div>
+                  </div>
+                <div class="subprojects">
+                    <img src="imgs/resized-p/render-2.jpg" alt="" width="300" height="300">
+                    <span>Motion & Illustration</span>
+                    <div class="curve">
+                      <a href="#">
+                        <button class="curve-box">                       
+                            <img src="imgs/svg/diagonal_arrow.svg" alt="">                       
+                        </button>
+                      </a> 
+                    </div>
+                  </div> 
+            </div>
+            <div class="sm-wd">
+                <div class="subprojects">
+                    <img src="imgs/resized-p/graphics-1.jpg" alt="" width="300" height="300">
+                    <span>Structual Mapping</span>
+                    <div class="curve">
+                      <a href="#">
+                        <button class="curve-box">                       
+                            <img src="imgs/svg/diagonal_arrow.svg" alt="">                       
+                        </button>
+                      </a> 
+                    </div>
+                  </div>
+                <div class="subprojects">
+                    <img src="imgs/resized-p/interior-3.jpg" alt="" width="300" height="300">
+                    <span>Interior Renders</span>
+                    <div class="curve">
+                      <a href="#">
+                        <button class="curve-box">                       
+                            <img src="imgs/svg/diagonal_arrow.svg" alt="">                       
+                        </button>
+                      </a> 
+                    </div>
+                  </div> 
+            </div>
+        </div>
+        <a href="#" class="bottom-target" style="color: purple; font-weight: 900; text-decoration: underline; display: block; margin-top: 3rem;">View all projects</a>
+     </section>
+     <div class="divider-middle">
+        <div class="custom-shape-divider-top">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+            </svg>
+        </div>
+     </div>
+     <div class="spacer"><img src="logos/ssstar.svg" alt="spacer"></div>
+     
+     <!-- testimonials -->
+     <section class="testimonials">
+      <h2> Our Clients Love US</h2>
+      <div id="slider" class="client-stack">
+          <!-- cards -->
+          <div class="c-card">
+              <div>
+                  <p>They are very effective and responsive in their work. I recommend working with them on complex javascript and Webflow projects.</p>
+              </div>
+              <div class="c-btm">
+                  <div class="ctn-img">
+                      <img src="imgs/profile/p1.jpg" alt="" width="50" height="50">
+                  </div>
+                  <div class="cbm">
+                      <span>Samantha Devon</span>
+                      <span>Spekk Smart Studios</span>
+                  </div>
+              </div>
+          </div>
+          <div class="c-card">
+              <div>
+                  <p>Syncei are the best professionals I've ever worked with. They delivered exactly what I asked for in a very short period – I can't recommend them enough.</p>
+              </div>
+              <div class="c-btm">
+                  <div class="ctn-img">
+                      <img src="imgs/profile/p2.jpg" alt="" width="50" height="50">
+                  </div>
+                  <div class="cbm">
+                      <span>Sparkle Brown</span>
+                      <span>Sparkling.co</span>
+                  </div>
+              </div>
+          </div>
+          <div class="c-card">
+              <div>
+                  <p>They did a great job implementing a solution with out-of-the-box thinking. If you need a javascript developer for webflow projects, take them!</p>
+              </div>
+              <div class="c-btm">
+                  <div class="ctn-img">
+                      <img src="imgs/profile/p3.jpg" alt="" width="50" height="50">
+                  </div>
+                  <div class="cbm">
+                      <span>Spoken Stone</span>
+                      <span>Stones stick</span>
+                  </div>
+              </div>
+          </div>
+          <div class="c-card">
+              <div>
+                  <p>They Coded a specific functionality to my website fast, and very well done! Provided a video of how to edit it myself as well; I highly recommend will be coming back in the future.</p>
+              </div>
+              <div class="c-btm">
+                  <div class="ctn-img">
+                      <img src="imgs/profile/p4.jpg" alt="" width="50" height="50">
+                  </div>
+                  <div class="cbm">
+                      <span>Rebecca Rose</span>
+                      <span>Rosarian Disco</span>
+                  </div>
+              </div>
+          </div>
+          <div class="c-card">
+              <div>
+                  <p>They did an excellent job, understood immediately what we needed and delivered very fast.</p>
+              </div>
+              <div class="c-btm">
+                  <div class="ctn-img">
+                      <img src="imgs/profile/p5.jpg" alt="" width="50" height="50">
+                  </div>
+                  <div class="cbm">
+                      <span>Janny Doe</span>
+                      <span>Artifact ground</span>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+  
+        <!-- CTA -->
+  <section class="cta">
+        <div class="cta-card">
+            <div class="backit"></div>
+            <div class="blur"></div>
+     
+      <h2>
+        Have Projects in Mind? <br> Let's Work Together
+      </h2>
+      
+      <h3>
+        Ready to transfrom your brand and diversify your reach?
+      </h3>
+  
+      <a href="#" class="btn btn-primary">
+        <button class="button">
+          <div class="dots_border"></div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            class="sparkle"
+          >
+            <path
+              class="path"
+              stroke-linejoin="round"
+              stroke-linecap="round"
+              stroke="black"
+              fill="black"
+              d="M14.187 8.096L15 5.25L15.813 8.096C16.0231 8.83114 16.4171 9.50062 16.9577 10.0413C17.4984 10.5819 18.1679 10.9759 18.903 11.186L21.75 12L18.904 12.813C18.1689 13.0231 17.4994 13.4171 16.9587 13.9577C16.4181 14.4984 16.0241 15.1679 15.814 15.903L15 18.75L14.187 15.904C13.9769 15.1689 13.5829 14.4994 13.0423 13.9587C12.5016 13.4181 11.8321 13.0241 11.097 12.814L8.25 12L11.096 11.187C11.8311 10.9769 12.5006 10.5829 13.0413 10.0423C13.5819 9.50162 13.9759 8.83214 14.186 8.097L14.187 8.096Z"
+            ></path>
+            <path
+              class="path"
+              stroke-linejoin="round"
+              stroke-linecap="round"
+              stroke="black"
+              fill="black"
+              d="M6 14.25L5.741 15.285C5.59267 15.8785 5.28579 16.4206 4.85319 16.8532C4.42059 17.2858 3.87853 17.5927 3.285 17.741L2.25 18L3.285 18.259C3.87853 18.4073 4.42059 18.7142 4.85319 19.1468C5.28579 19.5794 5.59267 20.1215 5.741 20.715L6 21.75L6.259 20.715C6.40725 20.1216 6.71398 19.5796 7.14639 19.147C7.5788 18.7144 8.12065 18.4075 8.714 18.259L9.75 18L8.714 17.741C8.12065 17.5925 7.5788 17.2856 7.14639 16.853C6.71398 16.4204 6.40725 15.8784 6.259 15.285L6 14.25Z"
+            ></path>
+            <path
+              class="path"
+              stroke-linejoin="round"
+              stroke-linecap="round"
+              stroke="black"
+              fill="black"
+              d="M6.5 4L6.303 4.5915C6.24777 4.75718 6.15472 4.90774 6.03123 5.03123C5.90774 5.15472 5.75718 5.24777 5.5915 5.303L5 5.5L5.5915 5.697C5.75718 5.75223 5.90774 5.84528 6.03123 5.96877C6.15472 6.09226 6.24777 6.24282 6.303 6.4085L6.5 7L6.697 6.4085C6.75223 6.24282 6.84528 6.09226 6.96877 5.96877C7.09226 5.84528 7.24282 5.75223 7.4085 5.697L8 5.5L7.4085 5.303C7.24282 5.24777 7.09226 5.15472 6.96877 5.03123C6.84528 4.90774 6.75223 4.75718 6.697 4.5915L6.5 4Z"
+            ></path>
+          </svg>
+            <span class="text_button">Get Started</span>
+            </button>
+        </a>
+        </div>
+    </section>
+</div>
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/shared/footer.html'; ?>
+    <script src="shared/nav.js"></script>
+    <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/gsap.min.js"></script>
+    <script src="scripts/index.js"></script>
+</body>
+</html>
